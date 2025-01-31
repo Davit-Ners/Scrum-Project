675013884f016bec7049f95761078c4fb20bd05f
Davit <davit.ners02@gmail.com>
2025-01-31 13:47:34 +0100

    Ajout function pour afficher si panier vide

--- scripts/panier.js
+++ scripts/panier.js
@@ -1,1 +1,25 @@
 // Javascript pour la partie 'Gestion de project'
+
+// Recuperation elements DOM
+const PanierDiv = document.getElementById('cart-pack-elem');
+const cartTitle = document.getElementById('cart-title');
+
+// Const et Let "test" pour ma partie
+// let cartIsEmpty = true;
+const cartElem = []
+
+// Fonctions
+
+function showCart(cartIsEmpty, cartElements) {
+    if (cartIsEmpty) {
+        cartTitle.textContent = 'Your Cart (0)';
+    }
+
+    else {
+        showCartElem(cartElements);
+    }
+}
+
+
+
+// showCart(true);
